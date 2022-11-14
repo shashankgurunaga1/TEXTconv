c6af47a38b77fdddc6f4e667577b96cf49c1b953
Shashank Gurunaga <98585901+shashankgurunaga1@users.noreply.github.com>
2022-11-14 07:49:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,26 +2,29 @@
 import React from 'react';
 import TextArea from './Components/TextArea';
 import Navbar from './Components/Navbar';
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+//import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import About from './Components/About';
 
 function App() {
   return (
     <>
-    <Router>
+    <//Router>
 
     
     
       
     <Navbar title="Text Conv."/>
-    <Routes>
+    </*Routes*/>
    
     
-    <Route path="/" element={<TextArea title="Enter text to analyze"/>}/>
-     <Route path="/about" element={<About/>} />
+    </*Route*/>
+    </*path/="/" */>
+    </*element={]*//>
+    <TextArea title="Enter text to analyze"/>}
+     </*Route path="/about" element={}*/ />
+       </*About*//>
     
-    </Routes>
-    </Router>
+    </*/Routes></Router*/>
     
     
     </>
